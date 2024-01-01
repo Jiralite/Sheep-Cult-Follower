e1e3656b77feea2186b0719e2932e678d7d73fa1
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-01-01 22:55:50 +0000

    refactor: Use another API

--- source/index.ts
+++ source/index.ts
@@ -1,6 +1,5 @@
 interface Env {
 	WEBHOOK_URL: string;
-	OPENAI_API_KEY: string;
 	USER_ID: string;
 }
 
@@ -9,36 +8,21 @@ const enum Time {
 	Evening,
 }
 
-type ChatCompletion = { choices: { message: { content: string } }[] };
-const AI_FACTS = ["Lie about something to me.", "Tell me something completely random or made up."] as const;
-const AI_FACTS_LENGTH = AI_FACTS.length;
+interface FactResponse {
+	text: string;
+}
 
-async function sendFact(time: Time, { WEBHOOK_URL, OPENAI_API_KEY, USER_ID }: Env) {
-	const completion = await fetch("https://api.openai.com/v1/chat/completions", {
-		method: "POST",
-		headers: {
-			Authorization: `Bearer ${OPENAI_API_KEY}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			messages: [
-				{ role: "user", content: `${AI_FACTS[Math.floor(Math.random() * AI_FACTS_LENGTH)]} Keep your response short.` },
-			],
-			model: "gpt-4-1106-preview",
-		}),
-	});
-
-	const body = (await completion.json()) as ChatCompletion;
-	const response = body.choices[0]!.message.content;
-
-	const content = response
-		? `${time === Time.Morning ? "Wakey wakey" : "Take this to bed"}, <@${USER_ID}>!\n>>> ${response}`
-		: `Good day, <@${USER_ID}>!`;
+async function sendFact(time: Time, { WEBHOOK_URL, USER_ID }: Env) {
+	const body = (await (await fetch("https://uselessfacts.jsph.pl/random.json?language=en")).json()) as FactResponse;
+	const { text: response } = body;
 
 	await fetch(WEBHOOK_URL, {
 		headers: { "Content-Type": "application/json" },
 		method: "POST",
-		body: JSON.stringify({ allowed_mentions: { parse: ["users"] }, content }),
+		body: JSON.stringify({
+			allowed_mentions: { parse: ["users"] },
+			content: `${time === Time.Morning ? "Wakey wakey" : "Take this to bed"}, <@${USER_ID}>!\n>>> ${response}`,
+		}),
 	});
 }
 
